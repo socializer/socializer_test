63e9efb525a5bd2ce36fb9c6fc7ead5284981b11
Dominic Goulet <dominic.goulet@gmail.com>
2011-12-07 19:26:09 -0500

    Fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,9 +11,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
   provider :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   
-  provider :twitter,  'zqUFjXJA2xOACKaQm3LPnw', '2IEZlZOb4LGF0l74edti8MF37Kk2ResXz39PVLVF3Y'  
-  provider :facebook, '190856774298116', '2f9deaa6204eaa296fdb167b278f765d', {:client_options => {:ssl => {:ca_path => "/home/zedash/etc/ssl/certs"}}}  
-  provider :linkedin, 'UxBhhDp-ByHa5c-HtPBYRd7Fbrb21G9L8-GUPe0y80jpETz1kkl-S6TmGz1VqwWv', 'MyfpmgPtLYjOtHkF95fji4IMRGsozrIT-fhKKcOuhCDok-RjmFmJ2SkU1gdSWdnC'
+  provider :twitter,  ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
   
   provider :identity, :model => Socializer::Identity
 end

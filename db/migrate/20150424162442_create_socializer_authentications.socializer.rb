609e73e4b7f369a53e771699bff2123d4bf74526
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-04-24 12:25:08 -0400

    update the migrations

--- db/migrate/20150424162442_create_socializer_authentications.socializer.rb
+++ db/migrate/20150424162442_create_socializer_authentications.socializer.rb
@@ -2,12 +2,15 @@
 class CreateSocializerAuthentications < ActiveRecord::Migration
   def change
     create_table :socializer_authentications do |t|
-      t.integer  :person_id
-      t.string   :provider
-      t.string   :uid
+      t.integer  :person_id, null: false
+      t.string   :provider,  null: false
+      t.string   :uid,       null: false
       t.string   :image_url
 
       t.timestamps null: false
     end
+
+    add_index :socializer_authentications, :person_id
+    add_index :socializer_authentications, :provider
   end
 end

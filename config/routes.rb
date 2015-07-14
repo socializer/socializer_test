ed83935f48865a2982b84e772a5aa78567ffdd94
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-07-14 14:43:56 -0400

    line is too long

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Socializer::Engine => "/"
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest
+  # priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -12,7 +13,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route
+  # (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
   # Example resource route with options:

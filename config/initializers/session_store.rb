3d9b52ef25e9312ab3b8979c37a83be08b3229eb
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-07-14 14:35:17 -0400

    config/initializers: line is too long

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_socializer_test_session'
+Rails.application.config.session_store :cookie_store,
+                                       key: '_socializer_test_session'

a5dd7bc6fd2356a06da29ea6a3ccb321c0df5e9a
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2016-02-23 19:29:21 -0500

    fix hash style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,10 +45,10 @@ Rails.application.configure do
   #   Bullet.bullet_logger = true
   #   Bullet.console = true
   #   # Bullet.growl = true
-  #   # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
-  #   #                 :password => 'bullets_password_for_jabber',
-  #   #                 :receiver => 'your_account@jabber.org',
-  #   #                 :show_online_status => true }
+  #   # Bullet.xmpp = { account: 'bullets_account@jabber.org',
+  #   #                 password: 'bullets_password_for_jabber',
+  #   #                 receiver: 'your_account@jabber.org',
+  #   #                 show_online_status: true }
   #   Bullet.rails_logger = true
   #   # Bullet.bugsnag = true
   #   # Bullet.airbrake = true

153815790ba615fc2a2eb5e6d833bc19abb97087
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2018-08-28 07:50:52 -0400

    update better_errors to

--- Gemfile
+++ Gemfile
@@ -56,7 +56,7 @@ group :development do
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.1"
 
-  gem "better_errors", "~> 2.4.0"
+  gem "better_errors", "~> 2.5.0"
   gem "binding_of_caller", "~> 0.8.0"
   gem "meta_request", "~> 0.6.0" # For RailsPanel
   gem "pry-rails"
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module SocializerTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_record.sqlite3.represent_boolean_as_integer = false
   end
 end

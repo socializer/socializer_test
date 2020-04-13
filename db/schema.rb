417ef470ef55964e0931edfb21145500ff7c2257
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2020-04-12 23:16:01 -0400

    copy migrations and run them

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_13_025955) do
 
   create_table "socializer_activities", force: :cascade do |t|
     t.integer "actor_id", null: false

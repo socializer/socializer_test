89f75d5921c0cbb5f0a865a4e43046cf8c302349
acmetech <info@acmetechnologygroup.com>
2023-07-01 12:18:58 -0400

    add the socializer engine assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= link socializer/application.css
+//= link socializer/socializer.css
+//= link socializer/socializer-font.css
+//= link socializer/application.js

88976fc4ce5703a02163f3d5410d3134963a15b0
acmetech <info@acmetechnologygroup.com>
2024-01-15 12:25:29 -0500

    Layout/LineLength: Line is too long.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -18,11 +19,14 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
-  # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
+  # Ensures that a master key has been made available in
+  # ENV["RAILS_MASTER_KEY"], config/master.key, or an environment key such as
+  # config/credentials/production.key. This key is used to decrypt credentials
+  # (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
+  # Disable serving static files from `public/`, relying on NGINX/Apache to do
+  # so instead.
   # config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
@@ -38,19 +42,23 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Store uploaded files on the local file system
+  # (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  # config.action_cable.allowed_request_origins = ["http://example.com", %r{http://example.*}]
 
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
+  # Assume all access to the app is happening through a SSL-terminating
+  # reverse proxy.
+  # Can be used together with config.force_ssl for Strict-Transport-Security
+  # and secure cookies.
   # config.assume_ssl = true
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # Force all access to the app over SSL, use Strict-Transport-Security, and
+  # use secure cookies.
   config.force_ssl = true
 
   # Log to STDOUT by default
@@ -61,22 +69,25 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Info include generic and useful information about system operation, but avoids logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII). If you
-  # want to log everything, set the level to "debug".
+  # Info include generic and useful information about system operation, but
+  # avoids logging too much information to avoid inadvertent exposure of
+  # personally identifiable information (PII). If you want to log everything,
+  # set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # Use a real queuing backend for Active Job (and separate queues per
+  # environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "railsdiff_production"
 
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure the email server for immediate delivery to
+  # raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
